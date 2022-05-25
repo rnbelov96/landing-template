1229454f4d9019b8dad5dfbb03b38dbc085efa38
Belov Roman <rnbelov96@gmail.com>
2022-05-25 10:54:37 +0300

    Добавляет шаблон для списка на превом экране

--- src/ts/slider.ts
+++ src/ts/slider.ts
@@ -1,30 +1,46 @@
 export {};
 
-const swiper = new Swiper('.slidersection__slider', {
-  slidesPerView: 3,
-  spaceBetween: 30,
-  loop: true,
-  centeredSlides: true,
+const descSwiper = new Swiper('.desc__slider', {
+  slidesPerView: 1,
+  spaceBetween: 25,
+  enabled: true,
   pagination: {
-    el: '.slidersection__slider-pagination',
-    clickable: true,
-  },
-  navigation: {
-    nextEl: '.slidersection__slider-btn_next',
-    prevEl: '.slidersection__slider-btn_prev',
+    el: '.desc__slider-pagination',
   },
   breakpoints: {
-    640: {
-      slidesPerView: 2,
-      spaceBetween: 20,
-    },
-    768: {
-      slidesPerView: 4,
-      spaceBetween: 40,
-    },
-    1024: {
-      slidesPerView: 5,
-      spaceBetween: 50,
+    650: {
+      slidesPerView: 3,
+      enabled: false,
+      spaceBetween: 0,
     },
   },
 });
+
+// const swiper = new Swiper('.slidersection__slider', {
+//   slidesPerView: 3,
+//   spaceBetween: 30,
+//   loop: true,
+//   centeredSlides: true,
+//   pagination: {
+//     el: '.slidersection__slider-pagination',
+//     clickable: true,
+//   },
+//   navigation: {
+//     nextEl: '.slidersection__slider-btn_next',
+//     prevEl: '.slidersection__slider-btn_prev',
+//   },
+//   breakpoints: {
+//     640: {
+//       slidesPerView: 2,
+//       spaceBetween: 20,
+//     },
+//     768: {
+//       slidesPerView: 4,
+//       spaceBetween: 40,
+//     },
+//     1024: {
+//       slidesPerView: 5,
+//       spaceBetween: 50,
+//     },
+//   },
+// });
